8f7ee7436fe5e524f31534454c7ef5c8a703d44c
yogeshwari-jhala <yogeshwarijhala1627@gmail.com>
2022-12-22 23:41:07 +0530

    template-driven-form

---
+++ TYPESCRIPT/Assignment4/TemplateDrivenForm/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { UserForm } from './user-forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+  
+})
+export class AppComponent {
+  arr:UserForm[]=[];
+  
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  email:string ="";
+  subscription : string ="";
+  password : string="";
+  submitted:boolean=false;
+  title = 'TemplateDrivenForm';
+  onSubmit(item:any)
+  {
+    let user = new UserForm();
+    user.email= item.email;
+    user.password=item.password;
+    user.subscription=item.subscription;
+    this.arr.push(user);
+    this.submitted=true;
+  }
+}
---
+++ TYPESCRIPT/Assignment4/TemplateDrivenForm/src/app/user-forms.ts
@@ -0,0 +1,5 @@
+export class UserForm{
+        email?:string;
+        subscription?:string;
+        password?:string;
+}
