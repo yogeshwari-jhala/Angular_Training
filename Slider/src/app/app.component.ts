3290a09ec1e42ccbe5b89f035f17589a530da2a6
yogeshwari-jhala <yogeshwarijhala1627@gmail.com>
2023-01-03 23:55:13 +0530

    slider

---
+++ Slider/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  disabled = false;
+  max = 100;
+  min = 0;
+  showTicks = false;
+  step = 1;
+  thumbLabel = false;
+  value = 0;
+}
